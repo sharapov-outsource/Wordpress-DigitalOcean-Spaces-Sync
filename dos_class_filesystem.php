271ce0c5202d0a2b6d1878418df51414a032047a
Alexander S. <alexander@sharapov.biz>
2024-03-20 12:49:04 +0600

    Fixing aws connection details

--- dos_class_filesystem.php
+++ dos_class_filesystem.php
@@ -1,32 +1,29 @@
 <?php
 
 use Aws\S3\S3Client;
-use League\Flysystem\AdapterInterface;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\AwsS3v3\AwsS3V3Adapter;
 use League\Flysystem\Filesystem;
+
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-class DOS_Filesystem {
+class DOS_Filesystem
+{
+    public static function get_instance($key, $secret, $container, $endpoint): Filesystem
+    {
+        $client = new S3Client([
+            'credentials' => [
+                'key' => $key,
+                'secret' => $secret,
+            ],
+            'bucket' => 'do-spaces',
+            'endpoint' => $endpoint,
+            'version' => 'latest',
+            // region means nothing for DO Spaces, but aws client may drop and error without it
+            'region' => 'us-east-1',
+        ]);
 
-  public static function get_instance ( $key, $secret, $container, $endpoint ) {
-
-    $client = S3Client::factory([
-      'credentials' => [
-        'key'    => $key,
-        'secret' => $secret,
-      ],
-      'bucket' => 'do-spaces',
-      'endpoint' => $endpoint, 
-      'version' => 'latest',
-      // region means nothing for DO Spaces, but aws client may drop and error without it
-      'region' => 'us-east-1',
-    ]);
-  
-    $connection = new AwsS3Adapter($client, $container);
-    $filesystem = new Filesystem($connection);
-  
-    return $filesystem;
-
-  }
+        $connection = new AwsS3V3Adapter($client, $container);
+        return new Filesystem($connection);
+    }
 
 }
